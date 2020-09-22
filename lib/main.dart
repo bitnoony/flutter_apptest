4d42a250c74b1dc4ab49fd3c250d338ac35f5481
noony <noony74@daum.net>
2020-09-22 19:07:47 +0900

    profile을 다시 원래대로 수정

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   //var now = Jiffy().format("yyyy년 MM월 dd일");
 
-  QuerySnapshot data;
+ // QuerySnapshot data;
   int _selectedIndex = 0;
   PageController _pageController = PageController();
 
@@ -78,7 +78,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    loadData();
+    //loadData();
     return Scaffold(
 
         body: PageView(
@@ -122,10 +122,10 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Future<void> loadData() async {
+/*  Future<void> loadData() async {
     data = await FirebaseFirestore.instance.collection("board").get();
     setState(() {});
-  }
+  }*/
 }
 
 class MainPage extends StatefulWidget {
--- lib/profile.dart
+++ lib/profile.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_apptest/viewpage.dart';
 
 class Profile extends StatefulWidget {
   @override
@@ -6,66 +8,80 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+ QuerySnapshot data;
+
+/*
+  Future<void> loadData() async {
+    data = await FirebaseFirestore.instance.collection("board").where('name', isEqualTo: '송빛누니').get();
+    setState(() {});
+  }
+*/
+
   @override
   Widget build(BuildContext context) {
+   // loadData();
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: Text("사용자 정보"),
         centerTitle: true,
       ),
-      body: ListView(
-        children: [
-          UserAccountsDrawerHeader(
-            currentAccountPicture: CircleAvatar(
-              backgroundImage: AssetImage('assets/noony_profile.png'),
-              backgroundColor: Colors.white,
-            ),
-            accountName: Text('송빛누니', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize:18),),
-            accountEmail: Text('bitnoony@gmail.com', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18),),
-            onDetailsPressed: () {
-              print('setting clicked');
-            },
-            arrowColor: Colors.red,
-            decoration: BoxDecoration(
-              color: Colors.amber,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(20.0),
-                bottomRight: Radius.circular(20.0),
+      body:
+              UserAccountsDrawerHeader(
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: AssetImage('assets/noony_profile.png'),
+                  backgroundColor: Colors.white,
+                ),
+                accountName: Text('송빛누니', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize:18),),
+                accountEmail: Text('bitnoony@gmail.com', style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18),),
+                onDetailsPressed: () {
+                  print('setting clicked');
+                },
+                arrowColor: Colors.red,
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20.0),
+                    bottomRight: Radius.circular(20.0),
+                  ),
+                  boxShadow: [BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 10.0,
+                    spreadRadius: 4.0,
+                  )],
+                ),
               ),
-              boxShadow: [BoxShadow(
-                color: Colors.grey,
-                blurRadius: 10.0,
-                spreadRadius: 4.0,
-              )],
-            ),
-          ),
-          ListTile(
-            leading: Icon(Icons.settings),
-            title: Text('변경'),
-            onTap: (){
 
-            },
-            trailing: Icon(Icons.add),
-          ),
-          ListTile(
-            leading: Icon(Icons.settings),
-            title: Text('변경'),
-            onTap: (){
+/*          FutureBuilder<QuerySnapshot>(
+              future: FirebaseFirestore.instance.collection("board").orderBy('time', descending: true).get(),
+              builder: (context, snapshot){
+                if(snapshot.hasData){
+                  return ListView(
+                    children: snapshot.data.docs.map((e) => Card(
+                      child: ListTile(
+                        onTap: (){
+                          Navigator.push(context,
+                              MaterialPageRoute(     // documentData.data["title"]
+                                  builder: (context)=> ViewPage(e.data()["title"],e.data()["content"],e.reference,e.data()["name"],e.data()["date"],e.data()["timeUpdate"])
+                              )
+                          );
+                        },
+                        leading: Text(e.data()["date"]),
+                        title: Text(e.data()["title"] ?? ""), //subtitle: Text(e.data()["content"] ?? ""),
+                        trailing: Text(e.data()["name"]),//Icon(Icons.arrow_forward),
+                        dense: true,
+                      ),
+                    )).toList(),
+                  );
+                }else{
+                  return Center(child: CircularProgressIndicator(),);
+                }
+              }),*/
 
-            },
-            trailing: Icon(Icons.add),
-          ),
-          ListTile(
-            leading: Icon(Icons.settings),
-            title: Text('변경'),
-            onTap: (){
-
-            },
-            trailing: Icon(Icons.add),
-          ),
-        ],
-      ),
     );
+
+
+
+
   }
 }
